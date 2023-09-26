b602ebb7cff80c86d42e15bf3b25d0a5e70f7adc
wan2daaa <wan2daaa@gmail.com>
2023-09-27 02:07:45 +0900

    refactor: 로그 추가

--- src/main/java/school/kku/repellingserver/member/controller/MemberLoginController.java
+++ src/main/java/school/kku/repellingserver/member/controller/MemberLoginController.java
@@ -3,19 +3,19 @@ package school.kku.repellingserver.member.controller;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import school.kku.repellingserver.jwt.service.TokenService;
-import school.kku.repellingserver.jwt.util.JwtTokenUtils;
 import school.kku.repellingserver.mail.service.MailService;
+import school.kku.repellingserver.member.domain.Member;
 import school.kku.repellingserver.member.dto.*;
 import school.kku.repellingserver.member.service.MemberService;
 
-import static school.kku.repellingserver.jwt.constants.TokenDuration.ACCESS_TOKEN_DURATION;
 import static school.kku.repellingserver.jwt.constants.TokenDuration.REFRESH_TOKEN_DURATION;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1")
@@ -44,7 +44,8 @@ public class MemberLoginController {
 
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody RegisterRequest registerRequest) {
-        memberService.register(registerRequest);
+        Member register = memberService.register(registerRequest);
+        log.info("member = {}", register);
 
         return ResponseEntity.ok().build();
     }
--- src/main/java/school/kku/repellingserver/member/domain/Member.java
+++ src/main/java/school/kku/repellingserver/member/domain/Member.java
@@ -4,11 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
