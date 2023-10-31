463ba66e24c94cc437e680ba41a80a393787d4ea
wan2daaa <wan2daaa@gmail.com>
2023-11-01 00:58:32 +0900

    feat: 상세페이지 api 구현

--- src/main/java/school/kku/repellingserver/repellent/repellentData/controller/RepellentDataController.java
+++ src/main/java/school/kku/repellingserver/repellent/repellentData/controller/RepellentDataController.java
@@ -1,19 +1,25 @@
 package school.kku.repellingserver.repellent.repellentData.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import school.kku.repellingserver.gateway.dto.RepellentDataRequest;
-import school.kku.repellingserver.member.domain.Member;
 import school.kku.repellingserver.repellent.repellentData.domain.RepellentData;
+import school.kku.repellingserver.repellent.repellentData.dto.DailyDetectionListResponse;
 import school.kku.repellingserver.repellent.repellentData.dto.DayByDetectionListResponse;
+import school.kku.repellingserver.repellent.repellentData.dto.HourByDetectionListResponse;
 import school.kku.repellingserver.repellent.repellentData.dto.MainPageDataResponse;
+import school.kku.repellingserver.repellent.repellentData.dto.ReDetectionMinutesAndRepellentSoundResponse;
 import school.kku.repellingserver.repellent.repellentData.service.RepellentDataService;
-
-import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -35,10 +41,34 @@ public class RepellentDataController {
     return ResponseEntity.ok("OK");
   }
 
-
   @GetMapping("/main")
   public ResponseEntity<MainPageDataResponse> getRepellentDataList(@RequestParam Long farmId) {
     return ResponseEntity.ok(repellentDataService.getRepellentDataList(farmId));
+  }
+
+  @GetMapping("/detail/group-farm/farm/{farmId}")
+  public ResponseEntity<List<DayByDetectionListResponse>> getDayByDetectionList(@PathVariable Long farmId) {
+    return ResponseEntity.ok(repellentDataService.getDayByDetectionList(farmId));
+  }
+
+  @GetMapping("/detail/group-time/farm/{farmId}")
+  public ResponseEntity<List<HourByDetectionListResponse>> getDayByTimeList(@PathVariable Long farmId) {
+    return ResponseEntity.ok(repellentDataService.getDayByTimeList(farmId));
+  }
+
+  @GetMapping("/detail/group-detection-device/farm/{farmId}")
+  public ResponseEntity<List<DailyDetectionListResponse>> getGroupByDetectionDevice(@PathVariable Long farmId) {
+    return ResponseEntity.ok(repellentDataService.getDailyByDetectionList(farmId));
+  }
+
+  @GetMapping("/detail/detection-device-id/{detectionDeviceId}")
+  public ResponseEntity<List<DayByDetectionListResponse>> getEachDayDetectionListByDetectionDeviceId(@PathVariable Long detectionDeviceId) {
+    return ResponseEntity.ok(repellentDataService.getEachDayDetectionListByDetectionDeviceId(detectionDeviceId));
+  }
+
+  @GetMapping("/detail/detection/detection-device-id/{detectionDeviceId}")
+  public ResponseEntity<List<ReDetectionMinutesAndRepellentSoundResponse>> getDetectionListByDetectionDeviceId(@PathVariable Long detectionDeviceId) {
+    return ResponseEntity.ok(repellentDataService.getDetectionListByDetectionDeviceId(detectionDeviceId));
   }
 
 }
--- src/main/java/school/kku/repellingserver/repellent/repellentData/service/RepellentDataService.java
+++ src/main/java/school/kku/repellingserver/repellent/repellentData/service/RepellentDataService.java
@@ -1,6 +1,8 @@
 package school.kku.repellingserver.repellent.repellentData.service;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,11 +11,12 @@ import school.kku.repellingserver.gateway.dto.RepellentDataRequest;
 import school.kku.repellingserver.gateway.repository.GatewayRepository;
 import school.kku.repellingserver.repellent.repellentData.domain.RepellentData;
 import school.kku.repellingserver.repellent.repellentData.domain.RepellentSound;
+import school.kku.repellingserver.repellent.repellentData.dto.DailyDetectionListResponse;
 import school.kku.repellingserver.repellent.repellentData.dto.DayByDetectionListResponse;
+import school.kku.repellingserver.repellent.repellentData.dto.HourByDetectionListResponse;
 import school.kku.repellingserver.repellent.repellentData.dto.MainPageDataResponse;
+import school.kku.repellingserver.repellent.repellentData.dto.ReDetectionMinutesAndRepellentSoundResponse;
 import school.kku.repellingserver.repellent.repellentData.repository.RepellentDataRepository;
-
-import java.util.List;
 import school.kku.repellingserver.repellent.repellentData.repository.RepellentSoundRepository;
 import school.kku.repellingserver.repellent.repellentDevice.domain.RepellentDevice;
 import school.kku.repellingserver.repellent.repellentDevice.repository.RepellentDeviceRepository;
@@ -29,7 +32,7 @@ public class RepellentDataService {
 
     public MainPageDataResponse getRepellentDataList(Long farmId) {
 
-        List<DayByDetectionListResponse> dayByDataList = repellentDataRepository.findRepellentDataByMemberGroupByFarm(
+        List<DayByDetectionListResponse> dayByDataList = repellentDataRepository.findRepellentDataByMemberWithDays(
             farmId);
 
         MainPageDataResponse response = repellentDataRepository.findRepellentDataByFarmGroupByRepellentSound(
@@ -114,4 +117,45 @@ public class RepellentDataService {
             );
         }
     }
+
+    public List<DayByDetectionListResponse> getDayByDetectionList(Long farmId) {
+        return repellentDataRepository.findRepellentDataByMemberWithDays(farmId);
+    }
+
+    public List<HourByDetectionListResponse> getDayByTimeList(Long farmId) {
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime twoHoursAgo = now.minusHours(2);
+        LocalDateTime fourHoursAgo = now.minusHours(4);
+        LocalDateTime sixHoursAgo = now.minusHours(6);
+        LocalDateTime eightHoursAgo = now.minusHours(8);
+
+        List<HourByDetectionListResponse> response = repellentDataRepository.findRepellentDataByMemberWithHoursBetweenTime(
+            farmId, now, twoHoursAgo);
+        List<HourByDetectionListResponse> response1 = repellentDataRepository.findRepellentDataByMemberWithHoursBetweenTime(
+            farmId, twoHoursAgo, fourHoursAgo);
+        List<HourByDetectionListResponse> response2 = repellentDataRepository.findRepellentDataByMemberWithHoursBetweenTime(
+            farmId, fourHoursAgo, sixHoursAgo);
+        List<HourByDetectionListResponse> response3 = repellentDataRepository.findRepellentDataByMemberWithHoursBetweenTime(
+            farmId, sixHoursAgo, eightHoursAgo);
+
+        response.addAll(response1);
+        response.addAll(response2);
+        response.addAll(response3);
+        return response;
+    }
+
+    public List<DailyDetectionListResponse> getDailyByDetectionList(Long farmId) {
+        return repellentDataRepository.getDailyStatsForDevice(
+            farmId);
+    }
+
+    public List<DayByDetectionListResponse> getEachDayDetectionListByDetectionDeviceId(Long detectionDeviceId) {
+        return repellentDataRepository.findEachDayDetectionListByDetectionDeviceId(detectionDeviceId);
+    }
+
+    public List<ReDetectionMinutesAndRepellentSoundResponse> getDetectionListByDetectionDeviceId(Long detectionDeviceId) {
+        return repellentDataRepository.findDetectionListByDetectionDeviceIdLimit4(detectionDeviceId);
+    }
+
 }
